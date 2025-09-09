f96db497cfb6bd815cc4846c1aac01984f52f249
Kaitlyn O'Connor <kvoconnor@stu.naperville203.org>
2025-09-08 21:36:50 -0500

    Finished Sieve

--- Chapter 15 Activities/Sieve/src/Sieve.java
+++ Chapter 15 Activities/Sieve/src/Sieve.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.util.*;
 
 /**
  * A program that implements the sieve of Eratosthenes.
@@ -13,7 +13,27 @@ public class Sieve
         int n = in.nextInt();
 
         // Your work goes here
-        . . .
+        Set<Integer> numbers = new HashSet<>();
+        for (int i=2; i<=n; i++) //populates the set with #'s 2-n
+        {
+            numbers.add(i);
+        }
+        
+        
+        for (int i=2; i<=n; i++) //removes all numbers except primes
+        {
+            Iterator<Integer> iter = numbers.iterator();
+            while (iter.hasNext())
+            {
+                Integer num = iter.next();
+                if (num != i && num%i==0)
+                {
+                    iter.remove();
+                }
+                //System.out.println(numbers);
+            }
+        }
+        System.out.println(numbers);
 
 
 
