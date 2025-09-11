3fb207cc2327f1a288b2446fc5bee4d199963fd7
Kaitlyn-OConnor <kvoconnor@stu.naperville203.org>
2025-09-11 13:11:52 -0500

    Finished Map Pair Share

--- Chapter 15 Activities/Gradebook/src/Gradebook.java
+++ Chapter 15 Activities/Gradebook/src/Gradebook.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
-. . .
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * A program to add, remove, modify or print
  * student names and grades.
@@ -10,7 +15,7 @@ public class Gradebook
     {
         Scanner in = new Scanner(System.in);
 
-        . . .
+        Map<String, String> students = new HashMap<>();
 
         boolean done = false;
         while(!done)
@@ -22,17 +27,38 @@ public class Gradebook
                 done = true;
             } else if (input.equals("A"))
             {
-                . . .
+                in.nextLine();
+                System.out.println("What is the student's name? ");
+                String name = in.nextLine();
+                System.out.println("What is the student's grade? ");
+                String grade = in.nextLine();
+                students.put(name, grade);
 
             } else if (input.equals("R"))
             {
-                . . .
+                in.nextLine();
+                System.out.println("What is the student's name? ");
+                String name = in.nextLine();
+                students.remove(name);
+
+
             } else if (input.equals("M"))
             {
-                . . .
+                in.nextLine();
+                System.out.println("What is the student's name? ");
+                String name = in.nextLine();
+                System.out.println("What is the student's new grade? ");
+                String grade = in.nextLine();
+                students.put(name, grade);
+
             } else if (input.equalsIgnoreCase("P"))
             {
-                . . .
+                Set<String> keys = students.keySet();
+                for (String key: keys)
+                {
+                    System.out.println(key + ": " + students.get(key));
+                }
+
             } else
             {
                 done = true;
