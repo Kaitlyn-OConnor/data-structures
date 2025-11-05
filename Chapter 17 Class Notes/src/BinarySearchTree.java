9563070520611f5054bdfda425f6c64fafcaab51
Kaitlyn V Oconnor <KVOconnor@stu.naperville203.org>
2025-11-05 13:35:14 -0600

    BinarySearchTree Notes Day 1

--- Chapter 17 Class Notes/src/BinarySearchTree.java
+++ Chapter 17 Class Notes/src/BinarySearchTree.java
@@ -12,7 +12,7 @@ public class BinarySearchTree
     */
     public BinarySearchTree()
     {   
-        
+        this.root = null;
     }
     
     /**
@@ -21,7 +21,15 @@ public class BinarySearchTree
     */
     public void add(Comparable obj) 
     {   
-        
+        Node newNode = new Node();
+        newNode.data = obj;
+        newNode.left = null;
+        newNode.right = null;
+
+        if (this.root == null)
+            this.root = newNode;
+        else
+            this.root.addNode(newNode); // will only add if there is already a root that equals something
     }
 
     /**
@@ -31,6 +39,17 @@ public class BinarySearchTree
     */
     public boolean find(Comparable obj)
     {
+        Node current = this.root;
+        while (current != null) // if get to bottom of a path current will become null and the thing won't be found we we return false
+        {
+            int diff = obj.compareTo(current.data);
+            if (diff == 0)
+                return true; // found what were looking for because compareTo is 0
+            else if (diff < 0)
+                current = current.left;
+            else
+                current = current.right;
+        }
         return false;
     }
     
@@ -41,7 +60,25 @@ public class BinarySearchTree
     */
     public void remove(Comparable obj)
     {
-        
+        Node toBeRemoved = this.root;
+        boolean found = false;
+
+        while (!found && toBeRemoved != null)
+        {
+            int diff = obj.compareTo(toBeRemoved.data);
+            if (diff == 0)
+                found = true;
+            else if (diff < 0)
+                toBeRemoved = toBeRemoved.left;
+            else 
+                toBeRemoved =  toBeRemoved.right;
+        }
+        if (!found)
+            return; // thing wanted to remove isn't there
+        else
+        {
+            
+        }
     }
     
     /**
@@ -67,7 +104,9 @@ public class BinarySearchTree
     */
     static class Node
     {   
-        
+        public Comparable data; // MUST BE comparable and not object because can't compare objects
+        public Node left;
+        public Node right;
 
         /**
             Inserts a new node as a descendant of this node.
@@ -75,7 +114,22 @@ public class BinarySearchTree
         */
         public void addNode(Node newNode)
         {   
-            
+            // if diff < 0, new Node is to the left of this node and if diff > 0 then its to the right of this node
+            int diff = newNode.data.compareTo(data);
+            if (diff < 0)
+            { // adds the new to left only if there isn't a node there already otherwise checks where it should put it for the left child node
+                if (left == null)
+                    left = newNode;
+                else
+                    left.addNode(newNode);
+            }
+            else if (diff > 0) // use else if so it doesn't include duplicates anywhere
+            {
+                if (right == null)
+                    right = newNode;
+                else    
+                    right.addNode(newNode);
+            }
         }
     }
 }
